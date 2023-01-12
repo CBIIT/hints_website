4241b3baf82b9239344f4221d2f3e132d16a0206
relbreedlove <76014120+relbreedlove@users.noreply.github.com>
2023-01-12 11:12:44 -0500

    rwb - cookie testing
    rwb - cookie testing

---
+++ zremovecookie.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="zremovecookie.aspx.vb" Inherits="zremovecookie" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Cookie tester/remover</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="Button1" runat="server" Text="Remove Cookie" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ zremovecookie.aspx.vb
@@ -0,0 +1,36 @@
+﻿
+Partial Class zremovecookie
+    Inherits System.Web.UI.Page
+
+
+    Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+        If Not Page.IsPostBack Then
+
+            Dim TermsUseCookie As HttpCookie = Request.Cookies("TermsAccepted")
+            If TermsUseCookie Is Nothing Then
+
+                Response.Write("<p>No Cookie</p>")
+            Else
+                If (Request.Cookies("TermsAccepted").Value IsNot Nothing) And Request.Cookies("TermsAccepted").Value = "Accepted" Then
+
+                    Response.Write("<p>Cookie ==== " & Request.Cookies("TermsAccepted").Value & "---</p>")
+                Else
+
+                    Response.Write("<p>Cookie ELse Check ==== " & Request.Cookies("TermsAccepted").Value & "---</p>")
+                End If
+            End If
+
+
+        End If
+
+
+    End Sub
+
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim nameCookie As HttpCookie = Request.Cookies("TermsAccepted")
+        nameCookie.Expires = DateTime.Now.AddDays(-1)
+        Response.Cookies.Add(nameCookie)
+        Response.Write("<H1>done - removed</h1>")
+    End Sub
+End Class
